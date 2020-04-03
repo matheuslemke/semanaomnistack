e9978b326b5af4c1dd98886adf0350c6f4871638
Anderson Matheus Lemke <anderson.lemke@supero.com.br>
2020-04-03 13:36:04 -0300

    Props and State

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function increment() {
+    counter++;
+  }
+
+  function incrementState() {
+    setCounter(++counter);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={incrementState}>Incrementar com state</button>
+    </div>
   );
 }
 
